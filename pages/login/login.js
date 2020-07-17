e0027d48572cd3491dd48da4e18156b7ad608a8c
szymc <zy991031>
2020-07-17 18:00:51 +0800

    index

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,96 +1,66 @@
+// pages/login/login.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    show: false,
-    minDate: new Date(2020, 6, 1).getTime(),
-    maxDate: new Date(2010, 6, 31).getTime(),
-    list: ['a', 'b'],
-    result: []
-  },
-  onDisplay() {
-    this.setData({ show: true });
-  },
-  onClose() {
-    this.setData({ show: false });
-  },
-  formatDate(date) {
-    date = new Date(date);
-    return `${date.getMonth() + 1}/${date.getDate()}`;
-  },
-  onConfirm(event) {
-    this.setData({
-      show: false,
-      date: this.formatDate(event.detail),
-    });
-  },
-  onChange(event) {
-    this.setData({
-      result: event.detail
-    });
-  },
-  toggle(event) {
-    const { index } = event.currentTarget.dataset;
-    const checkbox = this.selectComponent(`.checkboxes-${index}`);
-    checkbox.toggle();
+
   },
 
-  noop() {},
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   }
 })
